74b1f7d1c32c8874f028852cd42aa6ee58812941
Bruna Moraes <brunam.rocha26@gmail.com>
2020-10-18 20:10:46 -0300

    Images upload

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 import OrphagesController from './controllers/OrphanagesController';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/orphanages', OrphagesController.index);
-routes.post('/orphanages', OrphagesController.create);
+routes.get('/orphanages/:id', OrphagesController.show);
+routes.post('/orphanages', upload.array('images'), OrphagesController.create);
 
 export default routes;
